5579a024209e1f12e8a8d05aed4cdc7827f23a90
Manuel Raynaud <manu@raynaud.io>
2020-09-21 09:58:08 +0200

    🔧(setup) change release version according to last tag
    The release version used in setup.py file is not corresponding to the
    last tag. We must change it to version 1.0.3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def package_data(pkg, roots):
 
 setup(
     name='scormxblock-xblock',
-    version='0.5',
+    version='1.0.3',
     description='scormxblock XBlock',   # TODO: write a better description.
     packages=[
         'scormxblock',
